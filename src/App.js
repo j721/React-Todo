b2bff04c2177c3bc462d1a34afb095a6dc83efa8
j721 <juliadvu@gmail.com>
2020-04-06 14:39:32 -0500

    created array of todo task items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import TodoList from './components/TodoList';
+import Todoform from './components/TodoForm';
+// import '.components/Todo.css';
+
+
+const items = [
+  {
+      task: 'Clean bathroom',
+      id: 1,
+      completed: false
+  },
+  {
+      task: 'Clean kitchen',
+      id: 2,
+      completed: false
+  },
+  {
+      task: 'Clean bedroom',
+      id: 1,
+      completed: false
+  },
+  {
+      task: 'Clean out closet',
+      id: 1,
+      completed: false
+  },
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor(){
+    super();
+    this.state ={
+      items  // same as === items: items
+    };
+  }
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
