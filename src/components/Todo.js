551ab238ecc6763da8f86158e5a9ed952741646c
j721 <juliadvu@gmail.com>
2020-04-06 15:51:08 -0500

    Updates to todo.js file

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -4,7 +4,13 @@ import React from 'react';
 const Todo = props =>{
     return (
         <div
-        />
+            className ={`iitem${props.item.completed ? 'completed' : ''}`}
+            onClick ={()=> props.toggleItem(props.item.id)}>
+
+          <p>{props.item.name}</p>  
+
+        </div>
+      
     )
 }
 
